38b2360b39480cfc95d040dcdccd8d4d7a32918c
Lua40 <lua4027@thi.de>
2025-05-21 20:34:43 +0200

    implemented functions and included a smaller example for visualization

--- feature_selection/SelectFeaturesData.R
+++ feature_selection/SelectFeaturesData.R
@@ -1,7 +1,9 @@
-# Input:
-# - patient_data_matrix (input & processed CSV)
-# - gene_ids (numbers of relevant genes to choosen disease)
+library(dplyr)
 
+
+###
+# Input:
+# gene_ids (ids of genes that are relevant to the previously choosen disease)
 ###
 # example gene ids as nested vectors (name, id of one gene per inner vector)
 proto_gene_ids <- c(c("AKT serine/threonine kinase 3", 10000),
@@ -10,33 +12,10 @@ proto_gene_ids <- c(c("AKT serine/threonine kinase 3", 10000),
                     c("NDUFC2-KCTD14 readthrough", 100532726),
                     c("peptidylprolyl isomerase F", 10105),
                     c("ADAM metallopeptidase domain 10", 102),
-                    c("cyclin dependent kinase 5", 1020),
-                    c("proteasome 26S subunit, non-ATPase 14", 10213),
-                    c("TPTEP2-CSNK1E readthrough", 102800317),
-                    c("APC regulator of WNT signaling pathway 2", 10297),
-                    c("reticulon 3", 10313),
-                    c("tubulin alpha 1b", 10376),
-                    c("tubulin beta 3 class III", 10381),
-                    c("tubulin beta 4A class IVa", 10382),
-                    c("tubulin beta 4B class IVb", 10383),
-                    c("ATP synthase peripheral stalk subunit d", 10476),
-                    c("cytochrome c oxidase subunit NDUFA4-like", 107984365),
-                    c("ubiquinol-cytochrome c reductase, complex III subunit XI", 10975), # nolint: line_length_linter.
-                    c("P3R3URF-PIK3R3 readthrough", 110117499),
-                    c("ADRM1 26S proteasome ubiquitin receptor", 11047),
-                    c("frizzled class receptor 10", 11211),
-                    c("tubulin alpha 3e", 112714),
-                    c("cholinergic receptor muscarinic 1", 1128),
-                    c("cholinergic receptor muscarinic 3", 1131),
-                    c("cholinergic receptor muscarinic 5", 1133))
-proto_gene_ids_small <- proto_gene_ids[1:5]
+                    c("cyclin dependent kinase 5", 1020))
 ###
 # example gene ids as named vector ([vector]names -> gene names, values -> ids)
-proto_gene_ids_v2 <- c(
-  10000, 10023, 100506742, 100532726, 10105, 102, 1020,
-  10213, 102800317, 10297, 10313, 10376, 10381, 10382, 10383, 10476, 107984365,
-  10975, 110117499, 11047, 11211, 112714, 1128, 1131, 1133
-)
+proto_gene_ids_v2 <- c(10000, 10023, 100506742, 100532726, 10105, 102, 1020)
 names(proto_gene_ids_v2) <- c(
   "AKT serine/threonine kinase 3",
   "FRAT regulator of WNT signaling pathway 1",
@@ -44,27 +23,8 @@ names(proto_gene_ids_v2) <- c(
   "NDUFC2-KCTD14 readthrough",
   "peptidylprolyl isomerase F",
   "ADAM metallopeptidase domain 10",
-  "cyclin dependent kinase 5",
-  "proteasome 26S subunit, non-ATPase 14",
-  "TPTEP2-CSNK1E readthrough",
-  "APC regulator of WNT signaling pathway 2",
-  "reticulon 3",
-  "tubulin alpha 1b",
-  "tubulin beta 3 class III",
-  "tubulin beta 4A class IVa",
-  "tubulin beta 4B class IVb",
-  "ATP synthase peripheral stalk subunit d",
-  "cytochrome c oxidase subunit NDUFA4-like",
-  "ubiquinol-cytochrome c reductase, complex III subunit XI",
-  "P3R3URF-PIK3R3 readthrough",
-  "ADRM1 26S proteasome ubiquitin receptor",
-  "frizzled class receptor 10",
-  "tubulin alpha 3e",
-  "cholinergic receptor muscarinic 1",
-  "cholinergic receptor muscarinic 3",
-  "cholinergic receptor muscarinic 5"
+  "cyclin dependent kinase 5"
 )
-proto_gene_ids_v2_small <- proto_gene_ids_v2[1:5]
 ###
 # same as previous, just a few specifically choosen gene ids & names
 proto_gene_ids_specif <- c(5406, 4602, 3630, 10223, 27035, 10000, 10023)
@@ -78,8 +38,30 @@ names(proto_gene_ids_specif) <- c(
   "AKT serine/threonine kinase 3",
   "FRAT regulator of WNT signaling pathway 1"
 )
-print(proto_gene_ids_specif)
+
+
+
 ###
+# functions
+ensure_gene_ids_are_strings <- function(genes) {
+  # checking given gene ids to be strings (else problems with indexing later)
+  if (! is.character(genes)) {
+    # converting to strings
+    temp_names <- names(genes)
+    genes <- as.character(genes)
+    names(genes) <- temp_names
+  }
+  return(genes)
+}
+
+
+find_all_occurring_gene_ids <- function(rownames_data, genes) {
+  genes[genes %in% rownames_data]
+}
+
+filter_matrix_by_genes <- function(data, genes) {
+  data[genes, ]
+}
 
 
 
@@ -94,19 +76,40 @@ patient_data_matrix <- patient_data_results[[1]]
 patient_data_labels <- patient_data_results[[2]]
 gene_ids <- proto_gene_ids_specif
 
-###
-# checking given gene ids to be strings (else problems with indexing later)
-print(gene_ids)
-if (! is.character(gene_ids)) {
-  temp_names <- names(gene_ids)
-  gene_ids <- as.character(gene_ids)
-  names(gene_ids) <- temp_names
-  print(gene_ids)
-}
+gene_ids <- ensure_gene_ids_are_strings(gene_ids)
+
+occuring_gene_ids <- find_all_occurring_gene_ids(rownames(patient_data_matrix), gene_ids) # nolint: line_length_linter.
+patient_data_matrix_filtered <- filter_matrix_by_genes(patient_data_matrix, occuring_gene_ids) # nolint: line_length_linter.
+
+# small snippet to better visualize the filtering
+patient_data_matrix_snippet <- patient_data_matrix[, 1:2]
+patient_data_matrix_filtered_snippet <- filter_matrix_by_genes(patient_data_matrix_snippet, occuring_gene_ids) # nolint: line_length_linter.
+
 
 ###
-# filtering genes
-occuring_gene_ids <- gene_ids[gene_ids %in% rownames(patient_data_matrix)] # nolint: line_length_linter.
-print(occuring_gene_ids)
-patient_data_matrix_filtered <- patient_data_matrix[occuring_gene_ids, ]
-print(dim(patient_data_matrix_filtered))
+# function printing details to results (only for development)
+print_results_details <- function() {
+  # differences patient matrix
+  print("Using whole example data set")
+  print("[Dimension] Whole matrix before filtering")
+  print(dim(patient_data_matrix))
+  print("[Dimension] Resulting matrix after filtering")
+  print(dim(patient_data_matrix_filtered))
+  print("")
+  # small snippet of patient matrix to better visualize the filtering
+  print("Small snippet of example data set")
+  print("[Dimension + Data] Whole snippet matrix before filtering")
+  print(dim(patient_data_matrix_snippet))
+  print(patient_data_matrix_snippet)
+  print("[Dimension + Data] Resulting snippet matrix after filtering")
+  print(dim(patient_data_matrix_filtered_snippet))
+  print(patient_data_matrix_filtered_snippet)
+  print("")
+  # differences on the gene ids side
+  print("[Data] The given gene ids by the database query (later KEGG)")
+  print(gene_ids)
+  print("[Data] Filtered gene ids that actually occur in the patient data")
+  print(occuring_gene_ids)
+}
+
+print_results_details()
