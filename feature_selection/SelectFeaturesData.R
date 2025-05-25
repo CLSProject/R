bf862ed8d32a57d41d7ae92099bfb3e138930fd6
Lua40 <lua4027@thi.de>
2025-05-25 11:14:14 +0200

    adjusted variable names in the feature selection

--- feature_selection/SelectFeaturesData.R
+++ feature_selection/SelectFeaturesData.R
@@ -1,27 +1,29 @@
 ###
 # functions
-ensure_gene_ids_are_strings <- function(genes) {
+ensure_gene_ids_are_strings <- function(gen_ids) {
   # checking given gene ids to be strings (else problems with indexing later)
-  if (! is.character(genes)) {
+  if (! is.character(gen_ids)) {
     # converting to strings
-    temp_names <- names(genes)
-    genes <- as.character(genes)
-    names(genes) <- temp_names
+    temp_names <- names(gen_ids)
+    gen_ids <- as.character(gen_ids)
+    names(gen_ids) <- temp_names
   }
-  return(genes)
+  return(gen_ids)
 }
 
 
-find_all_occurring_genes <- function(rownames_data, genes, gene_names) {
-  mask_occurring <- genes %in% rownames_data
-  list(genes[mask_occurring], gene_names[mask_occurring])
+find_all_occurring_genes <- function(rownames_data, gen_ids, gene_names) {
+  mask_occurring <- gen_ids %in% rownames_data
+  list(gen_ids[mask_occurring], gene_names[mask_occurring])
 }
 
 
-get_filtered_matrix_genes <- function(data, genes, names) {
-  genes <- ensure_gene_ids_are_strings(genes)
+get_filtered_matrix_genes <- function(data, gen_ids, gen_names) {
+  gen_ids <- ensure_gene_ids_are_strings(gen_ids)
 
-  occuring_genes_results <- find_all_occurring_genes(rownames(data), genes)
+  occuring_genes_results <- find_all_occurring_genes(rownames(data),
+                                                     gen_ids,
+                                                     gen_names)
   data_filtered <- data[occuring_genes_results[[1]], ]
 
   list(data_filtered, occuring_genes_results[[1]], occuring_genes_results[[2]])
