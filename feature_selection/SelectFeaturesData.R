6e7bbbfb04c7041551598970975832422882b646
Lua40 <lua4027@thi.de>
2025-05-29 22:18:12 +0200

    adjusted to expected data structure and missing gen ids

--- feature_selection/SelectFeaturesData.R
+++ feature_selection/SelectFeaturesData.R
@@ -12,19 +12,27 @@ ensure_gene_ids_are_strings <- function(gen_ids) {
 }
 
 
-find_all_occurring_genes <- function(rownames_data, gen_ids, gene_names) {
-  mask_occurring <- gen_ids %in% rownames_data
-  list(gen_ids[mask_occurring], gene_names[mask_occurring])
+filter_usable_gen_ids <- function(gen_ids, limit = 0) {
+  gen_ids <- gen_ids[gen_ids != "0"]
+  # if given a limit of genes
+  if (limit > 0) {
+    gen_ids[1:min(limit, length(gen_ids))]
+  }
 }
 
 
-get_filtered_matrix_genes <- function(data, gen_ids, gen_names) {
+find_all_occurring_genes <- function(rownames_data, gen_ids) {
+  gen_ids[gen_ids %in% rownames_data]
+}
+
+
+get_filtered_matrix_genes <- function(data, gen_ids, limit = 0) {
   gen_ids <- ensure_gene_ids_are_strings(gen_ids)
+  gen_ids <- filter_usable_gen_ids(gen_ids, limit)
 
-  occuring_genes_results <- find_all_occurring_genes(rownames(data),
-                                                     gen_ids,
-                                                     gen_names)
-  data_filtered <- data[occuring_genes_results[[1]], ]
+  gen_ids_filtered <- find_all_occurring_genes(rownames(data),
+                                               gen_ids)
+  data_filtered <- data[gen_ids_filtered, ]
 
-  list(data_filtered, occuring_genes_results[[1]], occuring_genes_results[[2]])
+  list(data_filtered, gen_ids_filtered)
 }
