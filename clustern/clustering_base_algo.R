e414df9e67d23388b00d1fc8d0c5176cd7494be8
erh9779 <erh9779@thi.de>
2025-06-11 18:30:15 +0200

    accidentally pushed version restored

--- clustern/clustering_base_algo.R
+++ clustern/clustering_base_algo.R
@@ -21,31 +21,31 @@ agglomerative_clustering_base_algo<-function(dist,alpha_i=0.5,alpha_j=0.5,beta=0
     clusters <- lapply(1:n, function(i) -i)
     names(clusters)<-as.character(-(1:n))
     #labels<-colnames(data)
-
+    
     merge<-matrix(0,nrow = n-1,ncol=2)
     storage.mode(merge)<-"integer"
     height<-numeric(n-1)
-
+    
     cluster_id_counter<-1
-
+    
     while(length(clusters)>1){
       min_dist<-Inf
       pair<-c(NA,NA)
       cluster_names<-names(clusters)
-
+      
       for (i in 1:(length(clusters)-1)){
         for (j in(i+1): length(clusters)){
           ci_name <- names(clusters)[i]
           cj_name <- names(clusters)[j]
-
+          
           dists<-dist[ci_name,cj_name,drop=FALSE]
           link_dist<-min(dists)
-
+          
           if (link_dist<min_dist){
             min_dist<-link_dist
             pair<-c(ci_name,cj_name)
           }
-
+          
         }
       }
       ci_name <- pair[1]
@@ -53,16 +53,16 @@ agglomerative_clustering_base_algo<-function(dist,alpha_i=0.5,alpha_j=0.5,beta=0
       ci_points <- clusters[[ci_name]]
       cj_points <- clusters[[cj_name]]
       new_cluster_points <- c(ci_points, cj_points)
-
+      
       new_cluster_name <- as.character(cluster_id_counter)
 
-
+      
       merge[cluster_id_counter, ] <- c(as.integer(ci_name), as.integer(cj_name))
       height[cluster_id_counter] <- min_dist
-
+      
       cluster_i_size=length(clusters[[ci_name]])
       cluster_j_size=length(clusters[[cj_name]])
-
+      
       clusters_to_remove <- sort(pair, decreasing = TRUE)
       clusters[[clusters_to_remove[1]]] <- NULL
       clusters[[clusters_to_remove[2]]] <- NULL
@@ -79,11 +79,11 @@ agglomerative_clustering_base_algo<-function(dist,alpha_i=0.5,alpha_j=0.5,beta=0
       if (link_crit=="UPGMA"){
         alpha_i=cluster_i_size/(cluster_i_size+cluster_j_size)
         alpha_j=cluster_j_size/(cluster_i_size+cluster_j_size)
-
+        
       }
       if (length(not_fused_clusters)>0){
         for (i in 1:length(not_fused_clusters)){
-
+          
           h<-as.integer(not_fused_clusters[[i]])
           h_name<-names(not_fused_clusters)[i]
           d_hi<-dist[h_name,ci_name]
@@ -107,16 +107,16 @@ agglomerative_clustering_base_algo<-function(dist,alpha_i=0.5,alpha_j=0.5,beta=0
           new_dists[rownames(dist),colnames(dist)]<-dist[rownames(dist),colnames(dist)]
 
         }
-
+        
 
         dist<- new_dists
-
+        
         cluster_id_counter <- cluster_id_counter + 1
       }
     }
-
-
-
+    
+    
+  
   #hclust-Objekt nachahmen
   hc <- list(
     merge = merge,
@@ -127,27 +127,21 @@ agglomerative_clustering_base_algo<-function(dist,alpha_i=0.5,alpha_j=0.5,beta=0
     call = match.call(),
     dist.method = "dist_crit"
   )
-
+  
   class(hc) <- "hclust"
   return(hc)
 }
 
 cluster_both<-function(data,alpha_i,alpha_j,beta,gamma,dist_crit,link_crit=""){
-  yyy <- dist((t(data)),method=dist_crit)
-  cat("yyy", class(yyy), "\n")
-  print(str(yyy))
-
   dist_pat<-as.matrix(dist((t(data)),method=dist_crit))
-  cat("distpat", class(dist_pat), "\n")
   dist_gene<-as.matrix(dist(data,method=dist_crit))
   pat_clustering=agglomerative_clustering_base_algo(dist=dist_pat,alpha_i=alpha_i,alpha_j=alpha_j,beta=beta,gamma=gamma,link_crit=link_crit)
-  cat("pat_clustering", class(pat_clustering), "\n")
   gene_clustering=agglomerative_clustering_base_algo(dist=dist_gene,alpha_i=alpha_i,alpha_j=alpha_j,beta=beta,gamma=gamma,link_crit=link_crit)
   return(list(
     pat_clustering = pat_clustering,
     gene_clustering = gene_clustering
   ))
-
+    
 }
 z_score_norm<-function(x){
   return ((x-mean(x))/sd(x))
@@ -162,33 +156,33 @@ calc_params_flexible<-function(beta){
   return(c(alpha_i = alpha_i, alpha_j = alpha_j, beta = beta, gamma = gamma))
 }
 
-# load("TCGA_kidney_unnormalized.RData")
-# data=dataset$data
-# small_data=data[1:5,1:5]
-# #Beispielaufruf für Normierungen
-# scaled_data<-t(apply(small_data,1,z_score_norm))
-# scaled_data <- t(apply(small_data, 1, min_max_scale))
-# #beispiel UPGMA
-# #cluster_result<-cluster_both(data=small_data,alpha_i=0.5,alpha_j=0.5,beta=0,gamma=0.5,dist_crit="euclidean",link_crit="UPGMA")
-# dist=  dist_mat <- as.matrix(dist((t(small_data)), method = "euclidean"))
-# #merge=result_small$merge
-# dist<-c(0,1,2,9,13,1,0,5,10,10,2,5,0,5,13,9,10,5,0,4,13,10,13,4,0)
-# dim(dist)<-c(5,5)
-# #beispiel berechnung parameter für flexible strategie und funktionsaufruf
-# params=calc_params_flexible(beta=0.98)
-# #Mit distanzmatrix
-# result_flexible<-agglomerative_clustering_base_algo(dist=dist,alpha_i=params["alpha_i"],
-#                                                     alpha_j=params["alpha_j"],
-#                                                     beta=params["beta"],
-#                                                     gamma=params["gamma"])
-# #auf normierten gendaten, in beide richtungen clustern
-# result_flexible_both=cluster_both(scaled_data,alpha_i=params["alpha_i"],
-#                                   alpha_j=params["alpha_j"],
-#                                   beta=params["beta"],
-#                                   gamma=params["gamma"],dist_crit="euclidean")
-# result_upgma<-agglomerative_clustering_base_algo(dist=dist,link_crit="UPGMA")
-# hcl_upgma<-hclust(as.dist(dist), method="average",members=NULL)
-# hcl_test<-hclust(as.dist(dist), method="single",members=NULL)
-# hcl_complete<-hclust(as.dist(dist),method="complete",members=NULL)
-# merge_hclust<-hcl_test$merge
-# hcl_small<-hclust(dist(t(small_data)), method="single",members=NULL)
+load("TCGA_kidney_unnormalized.RData")
+data=dataset$data
+small_data=data[1:5,1:5]
+#Beispielaufruf für Normierungen
+scaled_data<-t(apply(small_data,1,z_score_norm))
+scaled_data <- t(apply(small_data, 1, min_max_scale))
+#beispiel UPGMA
+#cluster_result<-cluster_both(data=small_data,alpha_i=0.5,alpha_j=0.5,beta=0,gamma=0.5,dist_crit="euclidean",link_crit="UPGMA")
+dist=  dist_mat <- as.matrix(dist((t(small_data)), method = "euclidean"))
+#merge=result_small$merge
+dist<-c(0,1,2,9,13,1,0,5,10,10,2,5,0,5,13,9,10,5,0,4,13,10,13,4,0)
+dim(dist)<-c(5,5)
+#beispiel berechnung parameter für flexible strategie und funktionsaufruf 
+params=calc_params_flexible(beta=0.98)
+#Mit distanzmatrix
+result_flexible<-agglomerative_clustering_base_algo(dist=dist,alpha_i=params["alpha_i"],
+                                                    alpha_j=params["alpha_j"],
+                                                    beta=params["beta"],
+                                                    gamma=params["gamma"])
+#auf normierten gendaten, in beide richtungen clustern
+result_flexible_both=cluster_both(scaled_data,alpha_i=params["alpha_i"],
+                                  alpha_j=params["alpha_j"],
+                                  beta=params["beta"],
+                                  gamma=params["gamma"],dist_crit="euclidean")
+result_upgma<-agglomerative_clustering_base_algo(dist=dist,link_crit="UPGMA")
+hcl_upgma<-hclust(as.dist(dist), method="average",members=NULL)
+hcl_test<-hclust(as.dist(dist), method="single",members=NULL)
+hcl_complete<-hclust(as.dist(dist),method="complete",members=NULL)
+merge_hclust<-hcl_test$merge
+hcl_small<-hclust(dist(t(small_data)), method="single",members=NULL)
