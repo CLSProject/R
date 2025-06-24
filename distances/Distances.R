cbb96848618bdc03b1c7609d729638915830d34e
erh9779 <erh9779@thi.de>
2025-06-25 00:17:32 +0200

    method minkowski added, functionality tests expanded to all methods, functionality tests and performance test harmonized rebuild of for loops to applies started

--- distances/Distances.R
+++ distances/Distances.R
@@ -1,116 +1,118 @@
-# install necessary packages if not installed yet and load them
-  # if (!require(this.path))install.packages("this.path")
-  # library(this.path)
-# preset constants, but be aware they might be overwritten by following loading of scripts
-  # CONSTANT <- 3
-# load necessary scripts
-  # setwd(dirname(this.path()))
-  # source("Utilities.R")
-
-
-
 # construct dist
   dist <- function(data, method = "euclidean", diag = FALSE, upper = FALSE, p = 2) {
 
-  calculate_manhattan <- function(row_lower_number, row_higher_number) {
-    distance <- 0
-    for (col_number in 1:ncol(data)) {
-      distance <- distance + abs(data[row_lower_number, col_number] - data[row_higher_number, col_number])
-    }
-    return (distance)
-  }
-
-  calculate_euclidean <- function(row_lower_number, row_higher_number) {
-    distance <- 0
-    for (col_number in 1:ncol(data)) {
-      distance <- distance + (data[row_lower_number, col_number] - data[row_higher_number, col_number])^2
-    }
-    return (sqrt(distance))
-  }
-
-  calculate_maximum <- function(row_lower_number, row_higher_number) {
-    distance <- 0
-    for (col_number in 1:ncol(data)) {
-      distance <- max(distance, abs(data[row_lower_number, col_number] - data[row_higher_number, col_number]))
-    }
-    return (distance)
-  }
-
-  calculate_binary <- function(row_lower_number, row_higher_number) {
-    only_one_on     <- 0
-    at_least_one_on <- 0
-    for (col_number in 1:ncol(data)) {
-      if (xor((data[row_lower_number, col_number] != 0),   (data[row_higher_number, col_number] != 0))) only_one_on     <- only_one_on     + 1
-      if (    (data[row_lower_number, col_number] != 0) || (data[row_higher_number, col_number] != 0) ) at_least_one_on <- at_least_one_on + 1
-    }
-    return (only_one_on / at_least_one_on)
-  }
-
-  # calculate_canberra <- function(row_lower_number, row_higher_number) {
-  #   distance <- 0
-  #   for (col_number in 1:ncol(data)) {
-  #     if (!is.na(summand <- (abs(data[row_lower_number, col_number]  -     data[row_higher_number, col_number])) /
-  #                           (abs(data[row_lower_number, col_number]) + abs(data[row_higher_number, col_number]))  )) {
-  #       distance <- distance + summand
-  #     }
-  #     print(summand)
-  #   }
-  #   return (distance)
-  # }
-
-  # calculate_minkowski <- function(row_lower_number, row_higher_number) {
-  #   distance <- 0
-  #   for (col_number in 1:ncol(data)) {
-  #     distance <- distance + (data[row_lower_number, col_number] - data[row_higher_number, col_number])^p
-  #   }
-  #   return (distance^(1/p))
-  # }
-
-  calculate_distances <- function(data, method) {
-    dist <- c()
-    for (row_lower_number in  1:(nrow(data) - 1)) {
-      for (row_higher_number in (row_lower_number + 1): nrow(data)) {
-        # distance <- 0
-        # for (col_number in 1:ncol(data)) {
-        #   distance <- distance + abs(data[row_lower_number, col_number] - data[row_higher_number, col_number])
-        # }
-        # if (method == "manhattan") distance <- calculate_manhattan(row_lower_number, row_higher_number)
-        distance <- get(paste("calculate", method, sep = "_"))(row_lower_number, row_higher_number)
-        dist <- c(dist, distance)
+    calculate_manhattan <- function(row_lower_number, row_higher_number) {
+      distance <- 0
+      for (col_number in 1:ncol(data)) {
+        distance <- distance + abs(data[row_lower_number, col_number] - data[row_higher_number, col_number])
       }
+      return (distance)
     }
-    # dist <- c()
-    # for (row_lower_number in  1:(nrow(data) - 1)) {
-    #   dist <- c(dist, sapply((row_lower_number + 1):nrow(data)
-    #                         ,function(row_higher_number) {
-    #                            return (sum(sapply(1:ncol(data)
-    #                                              ,function(col_number) {
-    #                                                 return (abs(data[row_lower_number, col_number] - data[row_higher_number, col_number]))
-    #                                               }
-    #                                              )
-    #                                       )
-    #                                   )
-    #                          }
-    #                         )
-    #            )
+
+    calculate_euclidean <- function(row_lower_number, row_higher_number) {
+      distance <- 0
+      for (col_number in 1:ncol(data)) {
+        distance <- distance + (data[row_lower_number, col_number] - data[row_higher_number, col_number])^2
+      }
+      return (sqrt(distance))
+    }
+
+    calculate_maximum <- function(row_lower_number, row_higher_number) {
+      distance <- 0
+      for (col_number in 1:ncol(data)) {
+        distance <- max(distance, abs(data[row_lower_number, col_number] - data[row_higher_number, col_number]))
+      }
+      return (distance)
+    }
+
+    calculate_binary <- function(row_lower_number, row_higher_number) {
+      only_one_on     <- 0
+      at_least_one_on <- 0
+      for (col_number in 1:ncol(data)) {
+        if (xor((data[row_lower_number, col_number] != 0),   (data[row_higher_number, col_number] != 0))) only_one_on     <- only_one_on     + 1
+        if (    (data[row_lower_number, col_number] != 0) || (data[row_higher_number, col_number] != 0) ) at_least_one_on <- at_least_one_on + 1
+      }
+      return (only_one_on / at_least_one_on)
+    }
+
+    # calculate_canberra <- function(row_lower_number, row_higher_number) {
+    #   distance <- 0
+    #   for (col_number in 1:ncol(data)) {
+    #     print(data[row_lower_number, col_number])
+    #     print(data[row_higher_number, col_number])
+    #     if (!is.na(summand <- (abs(data[row_lower_number, col_number]  -     data[row_higher_number, col_number])) /
+    #                           (abs(data[row_lower_number, col_number]) + abs(data[row_higher_number, col_number]))  )) {
+    #       print(summand)
+    #       distance <- distance + summand
+    #     }
+    #   }
+    #   cat("distance", distance, "\n")
+    #   return (distance)
     # }
-    return (dist)
-  }
 
-  # define object
-    obj <- calculate_distances(data, method)
+    calculate_minkowski <- function(row_lower_number, row_higher_number) {
+      distance <- 0
+      for (col_number in 1:ncol(data)) {
+        distance <- distance + (abs(data[row_lower_number, col_number] - data[row_higher_number, col_number]))^p
+      }
+      return (distance^(1/p))
+    }
 
-  # set class
-    class(obj) <- "dist"
+    calculate_distances <- function(data, method) {
+      dist <- c()
+      for (row_lower_number in  1:(nrow(data) - 1)) {
+        for (row_higher_number in (row_lower_number + 1): nrow(data)) {
+          # distance <- 0
+          # for (col_number in 1:ncol(data)) {
+          #   distance <- distance + abs(data[row_lower_number, col_number] - data[row_higher_number, col_number])
+          # }
+          # if (method == "manhattan") distance <- calculate_manhattan(row_lower_number, row_higher_number)
+          distance <- get(paste("calculate", method, sep = "_"))(row_lower_number, row_higher_number)
+          dist     <- c(dist, distance)
+        }
+      }
+      return (dist)
+    }
 
-  # set attributes
-    attr(obj, "Size")   <- nrow(data)
-    attr(obj, "Labels") <- rownames(data)
-    attr(obj, "Diag")   <- diag
-    attr(obj, "Upper")  <- upper
-    attr(obj, "method") <- method
-    attr(obj, "p")      <- p
-    attr(obj, "call")   <- match.call()
+    # calculate_applied_euclidean <- function(row_lower_number, row_higher_number) {
+    #   distance <- 0
+    #   for (col_number in 1:ncol(data)) {
+    #     distance <- distance + (data[row_lower_number, col_number] - data[row_higher_number, col_number])^2
+    #   }
+    #   return (sqrt(distance))
+    # }
+    #
+    # calculate_applied_distances <- function(data, method) {
+    #   dist <- c()
+    #   lower_rows <- data[1, nrow(data) - 1]
+    #   apply(lower_rows
+    #        ,function(lower_row) {
+    #           higher_rows <- data[1, nrow(data) - 1]
+    #           apply(higher_rows,
+    #                ,function(higher_row) {
+    #                   distance <- get(paste("calculate_applied_", method, sep = "_"))(lower_row, higher_row)
+    #                   dist     <- c(dist, distance)
+    #                   return (dist)
+    #                 }
+    #                )
+    #         }
+    #        )
+    # }
+
+    # define object
+      obj <- calculate_distances(data, method)
+
+    # set class
+      class(obj) <- "dist"
+
+    # set attributes
+      attr(obj, "Size")   <- nrow(data)
+      attr(obj, "Labels") <- rownames(data)
+      attr(obj, "Diag")   <- diag
+      attr(obj, "Upper")  <- upper
+      attr(obj, "method") <- method
+      attr(obj, "p")      <- p
+      attr(obj, "call")   <- match.call()
 
     return(obj)
 
