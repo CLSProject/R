bc753f90609dd9ce3e10e8592add64aeb644327c
erh9779 <erh9779@thi.de>
2025-06-28 14:18:59 +0200

    support files removed, only implementation and 2 test scripts left

--- distances/Roxygenise.bat
+++
@@ -1,3 +0,0 @@
-del Class_Header.*
-"c:\Users\Anwender\AppData\Local\Programs\R\R-4.3.3\bin\x64\Rscript.exe" Roxygen.R
-"c:/Program Files/RStudio/resources/app/bin/quarto/bin/tools/pandoc.exe" Class_Header.html -o Class_Header.docx
