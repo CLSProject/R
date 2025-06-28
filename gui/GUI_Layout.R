7b82407f1909b1ccd02f3fd120ba3b40fb2e262b
juf2390 <juf2390@thi.de>
2025-06-28 11:59:02 +0200

    GSE Nummern entfernt, Ladeanzeige entfernt, Vollbildmodus angepasst,  Beta auf max 1 begrenzt

--- gui/GUI_Layout.R
+++ gui/GUI_Layout.R
@@ -44,30 +44,30 @@ ui <- fluidPage(
       selectInput("disease", "Select disease (1 out of 24)...", 
                   choices = c(
                     "Please Select..." = "SelectDesease",
-                    "GSE1297 – Alzheimer's Disease – Hippocampal CA1" = "GSE1297",
-                    "GSE5281 – Alzheimer's Disease – Entorhinal Cortex" = "GSE5281_1",
-                    "GSE5281 – Alzheimer's Disease – Hippocampus" = "GSE5281_2",
-                    "GSE5281 – Alzheimer's Disease – Visual Cortex" = "GSE5281_3",
-                    "GSE20153 – Parkinson's disease – Lymphoblasts" = "GSE20153",
-                    "GSE20291 – Parkinson's disease – Putamen" = "GSE20291",
-                    "GSE8762 – Huntington's disease – Blood" = "GSE8762",
-                    "GSE4107 – Colorectal Cancer – Mucosa" = "GSE4107",
-                    "GSE8671 – Colorectal Cancer – Colon (1)" = "GSE8671",
-                    "GSE9348 – Colorectal Cancer – Colon (2)" = "GSE9348",
-                    "GSE14762 – Renal Cancer – Kidney (1)" = "GSE14762",
-                    "GSE781 – Renal Cancer – Kidney (2)" = "GSE781",
-                    "GSE15471 – Pancreatic Cancer – Pancreas (1)" = "GSE15471",
-                    "GSE16515 – Pancreatic Cancer – Pancreas (2)" = "GSE16515",
-                    "GSE19728 – Glioma – Brain" = "GSE19728",
-                    "GSE21354 – Glioma – Brain & Spine" = "GSE21354",
-                    "GSE6956 – Prostate Cancer – Prostate (1)" = "GSE6956_1",
-                    "GSE6956 – Prostate Cancer – Prostate (2)" = "GSE6956_2",
-                    "GSE3467 – Thyroid Cancer – Thyroid (1)" = "GSE3467",
-                    "GSE3678 – Thyroid Cancer – Thyroid (2)" = "GSE3678",
-                    "GSE9476 – AML – Blood/Bone marrow" = "GSE9476",
-                    "GSE18842 – Lung Cancer – Lung (1)" = "GSE18842",
-                    "GSE19188 – Lung Cancer – Lung (2)" = "GSE19188",
-                    "GSE3585 – Cardiomyopathy – Heart" = "GSE3585"
+                    "Alzheimer's Disease – Hippocampal CA1" = "GSE1297",
+                    "Alzheimer's Disease – Entorhinal Cortex" = "GSE5281_1",
+                    "Alzheimer's Disease – Hippocampus" = "GSE5281_2",
+                    "Alzheimer's Disease – Visual Cortex" = "GSE5281_3",
+                    "Parkinson's disease – Lymphoblasts" = "GSE20153",
+                    "Parkinson's disease – Putamen" = "GSE20291",
+                    "Huntington's disease – Blood" = "GSE8762",
+                    "Colorectal Cancer – Mucosa" = "GSE4107",
+                    "Colorectal Cancer – Colon (1)" = "GSE8671",
+                    "Colorectal Cancer – Colon (2)" = "GSE9348",
+                    "Renal Cancer – Kidney (1)" = "GSE14762",
+                    "Renal Cancer – Kidney (2)" = "GSE781",
+                    "Pancreatic Cancer – Pancreas (1)" = "GSE15471",
+                    "Pancreatic Cancer – Pancreas (2)" = "GSE16515",
+                    "Glioma – Brain" = "GSE19728",
+                    "Glioma – Brain & Spine" = "GSE21354",
+                    "Prostate Cancer – Prostate (1)" = "GSE6956_1",
+                    "Prostate Cancer – Prostate (2)" = "GSE6956_2",
+                    "Thyroid Cancer – Thyroid (1)" = "GSE3467",
+                    "Thyroid Cancer – Thyroid (2)" = "GSE3678",
+                    "AML – Blood/Bone marrow" = "GSE9476",
+                    "Lung Cancer – Lung (1)" = "GSE18842",
+                    "Lung Cancer – Lung (2)" = "GSE19188",
+                    "Cardiomyopathy – Heart" = "GSE3585"
                   )),
       
       
@@ -84,7 +84,7 @@ ui <- fluidPage(
       fluidRow(
         column(3, numericInput("alpha_i", "alpha_i:", value = 0.5, step = 0.1)),
         column(3, numericInput("alpha_j", "alpha_j:", value = 0.5, step = 0.1)),
-        column(3, numericInput("beta", "beta:", value = 0, step = 0.1)),
+        column(3, numericInput("beta", "beta:", value = 0, step = 0.1, max = 0.99)),
         column(3, numericInput("gamma", "gamma:", value = 0.5, step = 0.1))
       ),
       
@@ -126,9 +126,9 @@ ui <- fluidPage(
           div(class = "float-button",
               actionButton("expand", " + ")
           ),
-          wellPanel(
+         wellPanel(
             h5("Teilbild"),
-            withSpinner(textOutput("test_result_normal"), type = 6),
+            textOutput("test_result_normal"),
           )
       ),
       
@@ -138,8 +138,8 @@ ui <- fluidPage(
             div(class = "float-button",
                 actionButton("collapse", " - ")
             ),
-            h3("Vollbild"),
-            withSpinner(textOutput("test_result_fullscreen"), type = 6),
+             h3("Vollbild"),
+            textOutput("test_result_normal"),
         )
       )
       
@@ -275,7 +275,7 @@ server <- function (input, output, session) {
       
     } else {
     
-    Sys.sleep(5)
+    #Sys.sleep(5)
     
     csv_datapath <<- input$file$datapath  #für Lucy
     val_distance <<- input$distance
@@ -302,7 +302,7 @@ server <- function (input, output, session) {
   })
 
   reload_result <- eventReactive(input$reload, {
-    Sys.sleep(5)
+    #Sys.sleep(5)
     
     val_n_clusters <<- input$n_clusters
     val_colorpattern <<- input$colorpattern
@@ -344,7 +344,7 @@ server <- function (input, output, session) {
     }
   })
   
-  output$test_result_fullscreen <- renderText({
+  'output$test_result_fullscreen <- renderText({
     if (input$submit > input$reload) {
       submit_result()
     } else if (input$reload > 0) {
@@ -352,7 +352,7 @@ server <- function (input, output, session) {
     } else {
       "Bitte Submit oder Reload drücken"
     }
-  })
+  })'
 }
 
 shinyApp(ui = ui, server = server)
